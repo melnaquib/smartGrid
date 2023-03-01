192bbb90bb8c65b8fd0765731c8e81f3a67cff1a
Mustafa Elnaquib <melnaquib@gmail.com>
2023-03-01 20:13:02 +0100

    billing demo

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20Mintable {
-       string public name = "My Token";
-       string public symbol = "MYT";
+contract MyToken is ERC20 {
+       constructor() ERC20("My Token", "MYT") {
+       }
+
 }
